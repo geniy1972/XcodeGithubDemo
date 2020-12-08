7756ce16398e94599e6d81793b63b4f197342745
Eugen Chernitsky <EugenChernitsky@coherentsolutions.com>
2020-12-08 13:47:42 +0300

    Initial Commit

---
+++ XcodeGithubDemo/XcodeGithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGithubDemoApp.swift
+//  XcodeGithubDemo
+//
+//  Created by Eugen Chernitsky on 12/8/20.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
